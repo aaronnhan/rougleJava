b37c240d7823ae463a94f0f48c138d845607f1ce
Aaron Nhan <aaronnhan11@gmail.com>
2022-12-12 01:21:15 -0800

    init rougle repository

---
+++ src/main/java/com/anhan/rougle/RougleApplication.java
@@ -0,0 +1,13 @@
+package com.anhan.rougle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RougleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RougleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anhan/rougle/RougleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anhan.rougle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RougleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
